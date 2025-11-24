7ac391288a47d1ca45fc65623fec87057c284071
Vinicius Gabriel Marinho <vinicius.g.marinho@hotmail.com>
2025-11-24 19:31:42 -0300

    Change: tentando resolver problema do eslint

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async rewrites() {
+    return [
+      {
+        source: '/api/monsters/:path*',
+        destination: 'http://localhost:8080/monsters/:path*',
 };
+
 
 export default nextConfig;
